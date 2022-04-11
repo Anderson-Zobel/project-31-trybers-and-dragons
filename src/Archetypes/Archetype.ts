0551e6f6279e6f4a85951af95d6ad7a3ac85c6ce
Anderson Zobel <anderson.zobels@gmail.com>
2022-04-11 12:06:26 -0300

    feat: requirement 4

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../interface/Energy';
+
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() { return this._name; }  
+  get special() { return this._special; }
+  get cost() { return this._cost; }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get energyType(): EnergyType; 
+}
--- tests/sources/Archetype.exists.ts
+++ tests/sources/Archetype.exists.ts
@@ -1,5 +1,5 @@
 import Archetype from '../../src/Archetypes';
-import { EnergyType } from '../../src/Energy';
+import { EnergyType } from '../../src/interface/Energy';
 class ArchetypeChild extends Archetype {
   get energyType(): EnergyType {
     throw new Error('Method not implemented.');
